5d38d130fe5b9fe6029809dfa67a574c9cf4e6f9
Nicolas Herrera <nherrera@frro.utn.edu.ar>
2026-09-15 23:49:57 -0300

    PRISMA AHORA FUNCIONA

--- Backend/prisma/migrations/20260915230840_movie_room/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `Movie_room` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `capacity` INTEGER NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
