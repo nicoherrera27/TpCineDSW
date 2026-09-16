5b8c60bf88ed8ad4fb2f2af05b2d6d70dceb25ef
Nicolas Herrera <nherrera@frro.utn.edu.ar>
2026-09-16 15:55:15 -0300

    carpeta back es la que va

---
+++ Back/src/app.ts
@@ -0,0 +1,32 @@
+import { prisma } from "./lib/prisma";
+import 'dotenv/config';
+async function main() {
+
+  const user = await prisma.user.create({
+    data: {
+      NombreUsuario: "Alice",
+      Contrasenia: "password",
+      Nombre: "Alice",
+      Apellido: "Smith",
+      Email: "alice@prisma.io",
+      FechaNacimiento: new Date("1990-01-01"),
+      FechaAlta: new Date(),
+     
+    },
+  });
+  console.log("Created user:", user);
+
+  // Fetch all users with their posts
+  const allUsers = await prisma.user.findMany({});
+  console.log("All users:", JSON.stringify(allUsers, null, 2));
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
